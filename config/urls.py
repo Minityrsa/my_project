594d05189ecb875182d7a373c66ef6be88b59a02
Minityrsa <stanevich08@mail.ru>
2026-09-21 22:51:51 +0700

    feat(home-page): Вынести функционал отображения главной страницы в приложении blog.

--- config/urls.py
+++ config/urls.py
@@ -15,16 +15,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.http import HttpResponse
 from django.urls import include, path
-
-
-
-def home_page_view(request):
-    return HttpResponse("<h1>Главная страница</h1>")
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home_page_view),
+    path('', include('blog.urls')),
 ]
